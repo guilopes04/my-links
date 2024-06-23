8030d83754abd055ead69a8c67918d680e6aad8c
Guilherme <guilhermelopes035@gmail.com>
2024-06-23 00:15:15 -0300

    feat: ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,43 @@
-import React, { useEffect } from 'react'
-import './App.css'
-import MainDiv from './components/MainDiv'
-import ButtonLink, { ButtonLinkProps } from './components/ButtonLink'
-import UserInfo from './components/UserInfo'
-
-interface User {
-  imageSrc: string
-  name: string
-  role: string
-  buttonLinks?: ButtonLinkProps[] // tornando buttonLinks opcional
-}
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import MyLinks from './pages/MyLinks'
 
 const App: React.FC = () => {
-  let apiCalled = false
-  useEffect(() => {
-    const api =
-      'https://rvpf4aupl8.execute-api.us-east-1.amazonaws.com/v1/site-access'
-    if (!apiCalled) {
-      fetch(api, {
-        method: 'POST'
-      })
-    }
-    apiCalled = true
-  }, [])
+  const [apiCalled, setApiCalled] = useState(false)
 
-  const user: User = {
-    imageSrc: process.env.PUBLIC_URL + '/user_photo.jpg', // Caminho da imagem na pasta public
-    name: 'Guilherme Lopes',
-    role: 'Desenvolvedor Back-End',
-    buttonLinks: [
-      {
-        text: 'Linkedin',
-        link: 'https://www.linkedin.com/in/guilherme-lopes-18ab09210/',
-        icon: 'linkedin'
-      },
-      {
-        text: 'GitHub',
-        link: 'https://github.com/guilopes04',
-        icon: 'github'
+  useEffect(() => {
+    const callApi = async () => {
+      try {
+        const api =
+          'https://rvpf4aupl8.execute-api.us-east-1.amazonaws.com/v1/site-access'
+
+        if (!apiCalled) {
+          const response = await fetch(api, {
+            method: 'POST'
+          })
+
+          if (response.ok) {
+            console.log('API call successful')
+          } else {
+            console.error('API call failed')
+          }
+
+          setApiCalled(true)
+        }
+      } catch (error) {
+        console.error('Error calling API:', error)
       }
-    ]
-  }
+    }
+
+    callApi()
+  }, [apiCalled])
 
   return (
-    <MainDiv>
-      <UserInfo
-        src={user.imageSrc}
-        alt="User Image"
-        name={user.name}
-        role={user.role}
-      ></UserInfo>
-      {user.buttonLinks &&
-        user.buttonLinks.map((buttonLink, index) => (
-          <ButtonLink
-            key={index}
-            text={buttonLink.text}
-            link={buttonLink.link}
-            icon={buttonLink.icon}
-          />
-        ))}
-    </MainDiv>
+    <Router>
+      <Switch>
+        <Route path="/" component={MyLinks} />
+      </Switch>
+    </Router>
   )
 }
 
---
+++ src/pages/MyLinks.tsx
@@ -0,0 +1,51 @@
+import ButtonLink, { ButtonLinkProps } from '../components/ButtonLink'
+import MainDiv from '../components/MainDiv'
+import UserInfo from '../components/UserInfo'
+
+export interface User {
+  imageSrc: string
+  name: string
+  role: string
+  buttonLinks?: ButtonLinkProps[] // tornando buttonLinks opcional
+}
+
+const user: User = {
+  imageSrc: process.env.PUBLIC_URL + '/user_photo.jpg', // Caminho da imagem na pasta public
+  name: 'Guilherme Lopes',
+  role: 'Desenvolvedor Back-End',
+  buttonLinks: [
+    {
+      text: 'Linkedin',
+      link: 'https://www.linkedin.com/in/guilherme-lopes-18ab09210/',
+      icon: 'linkedin'
+    },
+    {
+      text: 'GitHub',
+      link: 'https://github.com/guilopes04',
+      icon: 'github'
+    }
+  ]
+}
+
+const MyLinks: React.FC = () => {
+  return (
+    <MainDiv>
+      <UserInfo
+        src={user.imageSrc}
+        alt="User Image"
+        name={user.name}
+        role={user.role}
+      ></UserInfo>
+      {user.buttonLinks &&
+        user.buttonLinks.map((buttonLink, index) => (
+          <ButtonLink
+            key={index}
+            text={buttonLink.text}
+            link={buttonLink.link}
+            icon={buttonLink.icon}
+          />
+        ))}
+    </MainDiv>
+  )
+}
+export default MyLinks
